908cb89fa4ada6b2d01ad47c2f76b51c46d3a971
Deepak <deepak.kadarivelu@conpend.com>
2025-09-19 17:04:43 +0200

    React router + Shadcn setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/gld-calc" element={<App />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
